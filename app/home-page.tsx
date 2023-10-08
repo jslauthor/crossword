9f3cd8132e72057cda0a7ffcce3439bc3bae5f81
Leonard Souza <hey@leonardsouza.com>
2023-10-08 16:39:47 -0700

    Add cubes

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -1,46 +1,62 @@
 'use client';
 
+import styled from '@emotion/styled';
 import PuzzlePreview, {
   DifficultyEnum,
   ProgressEnum,
 } from 'components/composed/PuzzlePreview';
 import MenuWrapper from 'components/core/MenuWrapper';
 
+const PreviewContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 10px;
+  align-items: center;
+  padding: 0.75rem;
+  width: 100%;
+  max-width: var(--primary-app-width);
+`;
+
 export default function Home() {
   return (
     <MenuWrapper>
-      <PuzzlePreview
-        title="The Tempest"
-        author="Leonard Souza"
-        date="September 4, 2023"
-        isAiAssisted={true}
-        difficulty={DifficultyEnum.Easy}
-        previewState={ProgressEnum.ZeroPercent}
-      />
-      <PuzzlePreview
-        title="The Tempest"
-        author="Leonard Souza"
-        date="September 4, 2023"
-        isAiAssisted={false}
-        difficulty={DifficultyEnum.Medium}
-        previewState={ProgressEnum.TwentyFivePercent}
-      />
-      <PuzzlePreview
-        title="The Tempest"
-        author="Leonard Souza"
-        date="September 4, 2023"
-        isAiAssisted={true}
-        difficulty={DifficultyEnum.Hard}
-        previewState={ProgressEnum.SeventyFivePercent}
-      />
-      <PuzzlePreview
-        title="The Tempest"
-        author="Leonard Souza"
-        date="September 4, 2023"
-        isAiAssisted={false}
-        difficulty={DifficultyEnum.Medium}
-        previewState={ProgressEnum.Solved}
-      />
+      <PreviewContainer>
+        <PuzzlePreview
+          title="The Tempest"
+          author="Leonard Souza"
+          date="September 4, 2023"
+          isAiAssisted={true}
+          difficulty={DifficultyEnum.Easy}
+          previewState={ProgressEnum.ZeroPercent}
+        />
+        <PuzzlePreview
+          title="The Tempest"
+          author="Leonard Souza"
+          date="September 4, 2023"
+          isAiAssisted={false}
+          difficulty={DifficultyEnum.Medium}
+          previewState={ProgressEnum.TwentyFivePercent}
+        />
+        <PuzzlePreview
+          title="The Tempest"
+          author="Leonard Souza"
+          date="September 4, 2023"
+          isAiAssisted={true}
+          difficulty={DifficultyEnum.Hard}
+          previewState={ProgressEnum.SeventyFivePercent}
+        />
+        <PuzzlePreview
+          title="The Tempest"
+          author="Leonard Souza"
+          date="September 4, 2023"
+          isAiAssisted={false}
+          difficulty={DifficultyEnum.Medium}
+          previewState={ProgressEnum.Solved}
+        />
+      </PreviewContainer>
     </MenuWrapper>
   );
 }
