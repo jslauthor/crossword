0f816cf5f7c06db8fdc704872506a618e583f8f5
Leonard Souza <hey@leonardsouza.com>
2023-10-24 06:08:14 -0700

    Add use client to components

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,12 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
 import { SignUp } from '@clerk/nextjs';
 
-export default function Page() {
-  return <SignUp />;
-}
+const Page: React.FC = () => {
+  const path = usePathname();
+  const params = useSearchParams();
+  return <SignUp redirectUrl={params.get('redirect_url') ?? '/'} path={path} />;
+};
+
+export default Page;
--- components/core/Header/index.tsx
+++ components/core/Header/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import * as React from 'react';
 
 import { LitElement, html, css } from 'lit';
