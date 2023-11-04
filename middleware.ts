d2aefa8210dfd5bf74f6ffc643fa2a48fc063814
Leonard Souza <hey@leonardsouza.com>
2023-11-04 10:42:09 -0700

    Update prisma and middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,8 @@
 import { authMiddleware, redirectToSignIn, useAuth } from '@clerk/nextjs';
-import {
-  NextFetchEvent,
-  NextMiddleware,
-  NextRequest,
-  NextResponse,
-} from 'next/server';
+import prisma from 'lib/prisma';
 
 export default authMiddleware({
   publicRoutes: ['/(.*)/'],
-  beforeAuth: (req, evt) => {
-    console.log('beforeAuth', req);
-  },
   afterAuth: async (auth, req, evt) => {
     // Redirect to login is user is not logged in
     // if (!auth.userId && !auth.isPublicRoute) {
@@ -20,7 +12,6 @@ export default authMiddleware({
     console.log('afterAuth', auth);
     // Sync user to local DB
     if (auth.userId) {
-      // TODO: Cache this call
       const localUser = await prisma.user.findFirst({
         where: {
           clerkId: auth.userId,
@@ -42,39 +33,4 @@ export default authMiddleware({
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-  // matcher: ['/(api|trpc)(.*)'],
-  // matcher: ['/(.*)/'],
 };
-
-// type MiddlewareFactory = (next: NextMiddleware) => NextMiddleware;
-
-// export function stackMiddlewares(
-//   functions: MiddlewareFactory[] = [],
-//   index = 0
-// ): NextMiddleware {
-//   const current = functions[index];
-//   console.log(index);
-//   if (current) {
-//     const next = stackMiddlewares(functions, index + 1);
-//     return current(next);
-//   }
-//   return () => NextResponse.next();
-// }
-
-// export default stackMiddlewares([
-//   () => {
-//     console.log('authMiddleware');
-//     return authMiddleware({});
-//   },
-//   (next: NextMiddleware) =>
-//     async (request: NextRequest, _next: NextFetchEvent) => {
-//       const { userId } = useAuth();
-//       console.log('ID!!', userId);
-//       return next(request, _next);
-//     },
-// ]);
-
-// export const config = {
-//   // matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-//   matcher: ['/(api|trpc)(.*)'],
-// };
