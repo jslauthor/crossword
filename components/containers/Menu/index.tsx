bbd9c55652ad88d431db869125855a71733064d3
Leonard Souza <hey@leonardsouza.com>
2023-11-06 14:07:13 -0800

    Add signin and sign out

--- components/containers/Menu/index.tsx
+++ components/containers/Menu/index.tsx
@@ -1,17 +1,32 @@
 import React, { ReactNode, useCallback } from 'react';
 import MenuWrapper, { MenuWrapperProps } from 'components/core/Menu';
 import { useClerk } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
 
 interface MenuProps extends MenuWrapperProps {}
 
 const Menu: React.FC<MenuProps> = ({ centerLabel, children }) => {
+  const router = useRouter();
   const { signOut } = useClerk();
   const onSignOut = useCallback(() => {
     signOut();
   }, [signOut]);
 
+  const onSignIn = useCallback(() => {
+    router.push('/signin');
+  }, [router]);
+
+  const onSignUp = useCallback(() => {
+    router.push('/signup');
+  }, [router]);
+
   return (
-    <MenuWrapper centerLabel={centerLabel} onSignOutPressed={onSignOut}>
+    <MenuWrapper
+      centerLabel={centerLabel}
+      onSignOutPressed={onSignOut}
+      onSignUpPressed={onSignUp}
+      onSignInPressed={onSignIn}
+    >
       {children}
     </MenuWrapper>
   );
