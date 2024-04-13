8a6565602e1d2384c90e3b1e11df43167a3acc42
Leonard Souza <hey@leonardsouza.com>
2024-04-13 20:23:33 +0200

    Add error page for missing puzzle

---
+++ app/puzzle/[slug]/error.tsx
@@ -0,0 +1,64 @@
+'use client'; // Error components must be Client Components
+
+import Menu from 'components/containers/Menu';
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import { Link, Button } from '@nextui-org/react';
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: var(--primary-app-width);
+  padding: 0.75rem;
+  background: var(--primary-bg);
+`;
+
+const ErrorContainer = styled.div`
+  padding: 1rem;
+`;
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <Menu>
+      <Container>
+        <ErrorContainer>
+          <h2>
+            Could not find the puzzle! If you have played this puzzle before
+            then this is likely a server error. Please{' '}
+            <Link color="warning" href="mailto:info@crosscube.com">
+              contact support for help
+            </Link>
+            .
+          </h2>
+          <br />
+          <div>
+            Or you can{' '}
+            <Button
+              size="sm"
+              variant="bordered"
+              onClick={
+                // Attempt to recover by trying to re-render the segment
+                () => reset()
+              }
+            >
+              refresh the page
+            </Button>
+          </div>
+        </ErrorContainer>
+      </Container>
+    </Menu>
+  );
+}
