61495d88317871d89f17e5a77b14e5c7737550e2
Leonard Souza <hey@leonardsouza.com>
2024-04-24 05:55:20 -0700

    Update error

--- party/index.ts
+++ party/index.ts
@@ -81,7 +81,7 @@ export default class Server implements Party.Server {
             });
             if (user == null || puzzleId == null) {
               throw new Error(
-                `Save: User or puzzle not found! ${user?.id} ${puzzleId}`,
+                `Save: User or puzzle not found! ${clerkId} ${user?.id} ${puzzleId}`,
               );
             }
             const state = Buffer.from(encodeStateAsUpdateV2(yDoc));
