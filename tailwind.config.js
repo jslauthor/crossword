01b17909d52db9ba1d9dac1b255479550c13fdcd
Leonard Souza <hey@leonardsouza.com>
2024-06-15 16:03:01 -0700

    Make the nextui light theme dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,29 @@
 // tailwind.config.js
-import {nextui} from "@nextui-org/react";
+import { nextui, semanticColors } from '@nextui-org/react';
 
+console.log(semanticColors.dark);
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
     // ...
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()]
-}
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: semanticColors.dark,
+        },
+      },
+    }),
+  ],
+};
 
 export default config;
