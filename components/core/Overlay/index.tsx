f855c6999ac0a946a6281dc891705b1252cabcd8
Leonard Souza <hey@leonardsouza.com>
2024-08-01 19:25:31 -0700

    Update scrollable content and global styles

--- components/core/Overlay/index.tsx
+++ components/core/Overlay/index.tsx
@@ -17,9 +17,15 @@ const ModalContainer = styled(motion.div)`
 `;
 
 const ModalContent = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
   max-width: var(--primary-app-width);
+`;
+
+const ScrollableContent = styled.div`
+  overflow-y: auto;
+  height: 100%;
 `;
 
 const ModalHeader = styled.div`
@@ -76,7 +82,7 @@ const Overlay: React.FC<OverlayProps> = ({
               </Text>
             </ModalHeader>
             <HRule />
-            {children}
+            <ScrollableContent>{children}</ScrollableContent>
           </ModalContent>
         </ModalContainer>
       )}
