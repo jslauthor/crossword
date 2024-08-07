0984a4e878024a7b34e30b4f9b1273d6ab65212f
Leonard Souza <hey@leonardsouza.com>
2024-08-07 11:57:54 -0700

    Add posthog

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,19 @@
 'use client';
 
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+
 import { UserConfigStoreProvider } from 'lib/providers/user-config-provider';
 import StyledComponentsRegistry from 'lib/registry';
 import { UserConfigState } from 'lib/stores/user-config';
 import { ThemeProvider } from 'lib/utils/hooks/theme';
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? '',
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+  });
+}
 
 export function Providers({
   children,
@@ -13,10 +23,12 @@ export function Providers({
   userConfig: UserConfigState;
 }) {
   return (
-    <UserConfigStoreProvider initialState={userConfig}>
-      <StyledComponentsRegistry>
-        <ThemeProvider>{children}</ThemeProvider>
-      </StyledComponentsRegistry>
-    </UserConfigStoreProvider>
+    <PostHogProvider client={posthog}>
+      <UserConfigStoreProvider initialState={userConfig}>
+        <StyledComponentsRegistry>
+          <ThemeProvider>{children}</ThemeProvider>
+        </StyledComponentsRegistry>
+      </UserConfigStoreProvider>
+    </PostHogProvider>
   );
 }
